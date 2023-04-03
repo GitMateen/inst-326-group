67bec28628e06240c8e31dec9668c4a85d90eba0
GitMateen <123765504+GitMateen@users.noreply.github.com>
2023-04-03 19:46:50 -0400

    Update movies.py

--- movies.py
+++ movies.py
@@ -28,3 +28,17 @@ def easy_movies(ez_dict):
     ez_dict["2006"] = {"movie": "Cars", "rating": 4.7, "description": "Computer-animated sports comedy film by Pixar Animation Studios. The first Cars series movie."}
     ez_dict["1997"] = {"movie": "Titanic", "rating": 4.8, "description": "Action-packed romance set against the ill-fated maiden voyage of the Titanic."}
     return ez_dict
+
+
+def hard_movies(hard_dict):
+    """A dictionary of the most popular/likely to be known movies.
+    
+    Args:
+        hard_dict (dict): A dictionary of 'hard' movies with their rating, description, and year.
+        
+    Returns:
+        hard_dict (dict): All the hard movies
+    """
+    hard_dict["1995"] = {"movie": "friday", "rating": 4.1, "description": "It is friday and the main charachter has just been fired."}
+    hard_dict["1991"] = {"movie": "boyz n the hood", "rating": 3.8, "description": "Group of friends trying to make it through growing up in crenshaw LA."}
+    return hard_dict
