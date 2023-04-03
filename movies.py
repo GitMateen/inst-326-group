2d72476351031946c331b73e8edd0a205603ef0b
Romeo Alvarenga <romeoa@Romeos-MBP.lan>
2023-04-03 17:44:42 -0400

    Added a function of easy movies. I also added docstrings to it

--- movies.py
+++ movies.py
@@ -15,3 +15,16 @@ def example_dict(our_dict):
     our_dict["2023"] = {"movie": "Creed 3", "rating": 5, "description": "The third creed movie, great boxing series and I would highly recommend watching."}
     our_dict["2012"] = {"movie": "The Avengers", "rating": 5, "description": "The first avengers movie, great action."}
     return our_dict
+
+def easy_movies(ez_dict):
+    """A dictionary of the most popular/likely to be known movies.
+    
+    Args:
+        ez_dict (dict): A dictionary of 'easy' movies with their rating, description, and year.
+        
+    Returns:
+        ez_dict (dict): All the easy movies
+    """
+    ez_dict["2006"] = {"movie": "Cars", "rating": 4.7, "description": "Computer-animated sports comedy film by Pixar Animation Studios. The first Cars series movie."}
+    ez_dict["1997"] = {"movie": "Titanic", "rating": 4.8, "description": "Action-packed romance set against the ill-fated maiden voyage of the Titanic."}
+    return ez_dict
