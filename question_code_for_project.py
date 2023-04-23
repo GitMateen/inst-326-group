59855e1472562e6be8e907e38ee151198066d114
GitMateen <123765504+GitMateen@users.noreply.github.com>
2023-04-23 18:13:27 -0400

    Update question_code_for_project.py

--- question_code_for_project.py
+++ question_code_for_project.py
@@ -4,38 +4,43 @@
                 "What movie features a young lion named Simba and his journey to become king of the Pride Lands?",
                 "What movie is based on the story of a wooden puppet who dreams of becoming a real boy?",
                 "What movie follows the adventures of a group of monsters who work at a company that scares children for a living?",
-                "What is the ",
-                "What"
+                "Which movie features a girl who travels over a rainbow to a magical land called Oz?",
+                "Which movie features a group of high school students who spend a day in detention together?"
             ],
             "medium": [
                 "What movie is based on the true story of an African-American football coach who takes over a high school team in Virginia during segregation?",
                 "What movie follows the story of a young girl named Chihiro who is transported to a magical world where she must work in a bathhouse for spirits to save her parents?",
                 "In what movie does a group of teenagers in detention discover a mysterious board game that transports them into a jungle adventure?",
-                "What is the",
-                "Who was the "
+                "In the movie Back to the Future, what is the name of the time machine?",
+                "Which movie features a character named Neo, who discovers he is living in a simulated reality?", 
+                "Which movie features a retired CIA agent who must protect his family from a group of assassins?"
+             
             ],
             "hard": [
                 "What was the top grossing movie in 2022",
                 "Who is the character in the movie 12 years a slave who was a free man but kidnapped and sold into slavery?",
-                "What",
-                "What",
-                "Who"
+                "who directed avatar the movie (2009)?",
+                "In the movie American Psycho, what is the name of the character played by Christian Bale?",
+                "In the movie Parasite, what is the name of the family who cons their way into working for a wealthy family?",
+                "In the movie The Wolf of Wall Street, what is the name of the character played by Leonardo DiCaprio?"
             ]
         }
         self.answers = {
             "The Lion King": "easy",
             "Pinocchio": "easy",
             "Monsters, Inc.": "easy",
-            "": "easy",
-            "": "easy",
+            "The Wizard of Oz": "easy",
+            "The Breakfast Club": "easy",
             "Remember the Titans": "medium",
             "Spirited Away": "medium",
             "Jumanji": "medium",
-            "": "medium",
-            " ": "medium",
+            "The DeLorean": "medium",
+            "The Matrix": "medium",
+            "Taken": "medium",
             "Top Gun: Maverick": "hard",
             "Solomon Northup": "hard",
-            "": "hard",
-            "": "hard",
-            "": "hard"
+            "James Cameron": "hard",
+            "Patrick Bateman": "hard",
+            "Kim": "hard"
+            "Jordan Belfort": "hard"
         }
