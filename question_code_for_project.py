4ff09c95539a2d2671d12dbc07d0324f47431fa6
Emmanuel Akpalu <emmanuelakpalu4580@gmail.com>
2023-04-27 18:19:17 -0400

    questions

--- question_code_for_project.py
+++ question_code_for_project.py
@@ -1,41 +1,42 @@
-
- self.questions = {
-            "easy": [
-                "What movie features a young lion named Simba and his journey to become king of the Pride Lands?",
-                "What movie is based on the story of a wooden puppet who dreams of becoming a real boy?",
-                "What movie follows the adventures of a group of monsters who work at a company that scares children for a living?",
-                "What is the ",
-                "What"
-            ],
-            "medium": [
-                "What movie is based on the true story of an African-American football coach who takes over a high school team in Virginia during segregation?",
-                "What movie follows the story of a young girl named Chihiro who is transported to a magical world where she must work in a bathhouse for spirits to save her parents?",
-                "In what movie does a group of teenagers in detention discover a mysterious board game that transports them into a jungle adventure?",
-                "What is the",
-                "Who was the "
-            ],
-            "hard": [
-                "What was the top grossing movie in 2022",
-                "Who is the character in the movie 12 years a slave who was a free man but kidnapped and sold into slavery?",
-                "What",
-                "What",
-                "Who"
-            ]
-        }
+class Questions:
+    def __init__(self):
+        self.questions = {
+                    "easy": [
+                        "What movie features a young lion named Simba and his journey to become king of the Pride Lands?",
+                        "What movie is based on the story of a wooden puppet who dreams of becoming a real boy?",
+                        "What movie follows the adventures of a group of monsters who work at a company that scares children for a living?",
+                        "What is the ",
+                        "What"
+                    ],
+                    "medium": [
+                        "What movie is based on the true story of an African-American football coach who takes over a high school team in Virginia during segregation?",
+                        "What movie follows the story of a young girl named Chihiro who is transported to a magical world where she must work in a bathhouse for spirits to save her parents?",
+                        "In what movie does a group of teenagers in detention discover a mysterious board game that transports them into a jungle adventure?",
+                        "What is the",
+                        "Who was the "
+                    ],
+                    "hard": [
+                        "What was the top grossing movie in 2022",
+                        "Who is the character in the movie 12 years a slave who was a free man but kidnapped and sold into slavery?",
+                        "What",
+                        "What",
+                        "Who"
+                    ]
+                }
         self.answers = {
-            "The Lion King": "easy",
-            "Pinocchio": "easy",
-            "Monsters, Inc.": "easy",
-            "": "easy",
-            "": "easy",
-            "Remember the Titans": "medium",
-            "Spirited Away": "medium",
-            "Jumanji": "medium",
-            "": "medium",
-            " ": "medium",
-            "Top Gun: Maverick": "hard",
-            "Solomon Northup": "hard",
-            "": "hard",
-            "": "hard",
-            "": "hard"
-        }
+                    "The Lion King": "easy",
+                    "Pinocchio": "easy",
+                    "Monsters, Inc.": "easy",
+                    "": "easy",
+                    "": "easy",
+                    "Remember the Titans": "medium",
+                    "Spirited Away": "medium",
+                    "Jumanji": "medium",
+                    "": "medium",
+                    " ": "medium",
+                    "Top Gun: Maverick": "hard",
+                    "Solomon Northup": "hard",
+                    "": "hard",
+                    "": "hard",
+                    "": "hard"
+                }
