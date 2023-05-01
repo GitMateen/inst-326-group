8bf96abeb82f00086bd1512d9e9e110227241c5d
GitMateen <123765504+GitMateen@users.noreply.github.com>
2023-05-01 11:56:14 -0400

    Update question_code_for_project.py
    fixed questioning format

--- question_code_for_project.py
+++ question_code_for_project.py
@@ -1,46 +1,78 @@
 questions = {
-            "easy": [
-                "What movie features a young lion named Simba and his journey to become king of the Pride Lands?",
-                "What movie is based on the story of a wooden puppet who dreams of becoming a real boy?",
-                "What movie follows the adventures of a group of monsters who work at a company that scares children for a living?",
-                "Which movie features a girl who travels over a rainbow to a magical land called Oz?",
-                "Which movie features a group of high school students who spend a day in detention together?"
-            ],
-            "medium": [
-                "What movie is based on the true story of an African-American football coach who takes over a high school team in Virginia during segregation?",
-                "What movie follows the story of a young girl named Chihiro who is transported to a magical world where she must work in a bathhouse for spirits to save her parents?",
-                "In what movie does a group of teenagers in detention discover a mysterious board game that transports them into a jungle adventure?",
-                "In the movie Back to the Future, what is the name of the time machine?",
-                "Which movie features a character named Neo, who discovers he is living in a simulated reality?", 
-                "Which movie features a retired CIA agent who must protect his family from a group of assassins?"
-            ],
-            "hard": [
-                "What was the top grossing movie in 2022?",
-                "Who is the character in the movie 12 years a slave who was a free man but kidnapped and sold into slavery?",
-                "Who directed Avatar the movie (2009)?",
-                "In the movie American Psycho, what is the name of the character played by Christian Bale?",
-                "In the movie Parasite, what is the name of the family who cons their way into working for a wealthy family?",
-                "In the movie The Wolf of Wall Street, what is the name of the character played by Leonardo DiCaprio?"
-            ]
-        }
+        "easy": [
+            "What movie features a young lion named Simba and his journey to become king of the Pride Lands?",
+            "What movie is based on the story of a wooden puppet who dreams of becoming a real boy?",
+            "What movie follows the adventures of a group of monsters who work at a company that scares children for a living?",
+            "Which movie features a girl who travels over a rainbow to a magical land called Oz?",
+            "Which movie features a group of high school students who spend a day in detention together?",
+            "What movie tells the story of a clownfish named Marlin searching for his son Nemo?",
+            "In what movie does a forgetful fish named Dory go on a journey to find her family?",
+            "What movie is about a young boy who finds a giant robot from outer space and helps him return home?",
+            "What movie features a team of superheroes, including Iron Man, Captain America, and Thor, who come together to save the world?",
+            "What movie is about a friendly alien who is stranded on Earth and befriends a young boy named Elliott?"
+        ],
+        "medium": [
+            "What movie is based on the true story of an African-American football coach who takes over a high school team in Virginia during segregation?",
+            "What movie follows the story of a young girl named Chihiro who is transported to a magical world where she must work in a bathhouse for spirits to save her parents?",
+            "In what movie does a group of teenagers in detention discover a mysterious board game that transports them into a jungle adventure?",
+            "In the movie Back to the Future, what is the name of the time machine?",
+            "Which movie features a character named Neo, who discovers he is living in a simulated reality?",
+            "Which movie features a retired CIA agent who must protect his family from a group of assassins?",
+            "What movie tells the story of a group of astronauts who travel through a wormhole in search of a new home for humanity?",
+            "In what movie does a young girl named Mathilda seek revenge against the corrupt DEA agent who killed her family?",
+            "What movie is about a man named Forrest Gump and his journey through life, from a low IQ individual to a war hero and a successful businessman?",
+            "In the movie The Truman Show, what is the name of the character played by Jim Carrey, who discovers his entire life is a TV show?"
+        ],
+        "hard": [
+            "What was the top grossing movie of all time as of 2021?",
+            "Who is the character in the movie 12 years a slave who was a free man but kidnapped and sold into slavery?",
+            "Who directed Avatar the movie (2009)?",
+            "In the movie American Psycho, what is the name of the character played by Christian Bale?",
+            "In the movie Parasite, what is the name of the family who cons their way into working for a wealthy family?",
+            "In the movie The Wolf of Wall Street, what is the name of the character played by Leonardo DiCaprio?",
+            "What movie tells the story of a young hacker named Lisbeth Salander who investigates a wealthy family's dark past?",
+            "In what movie does a man named Theodore fall in love with an artificially intelligent operating system named Samantha?",
+            "What movie is about a group of magicians who perform bank heists during their shows?",
+            "In the movie Blade Runner, what is the name of the special police unit tasked with hunting down rogue replicants?"
+        ]}
 answers = {
-          "The Lion King": "easy",
-          "Pinocchio": "easy",
-          "Monsters, Inc.": "easy",
-          "The Wizard of Oz": "easy",
-          "The Breakfast Club": "easy",
-          "Remember the Titans": "medium",
-          "Spirited Away": "medium",
-          "Jumanji": "medium",
-          "The DeLorean": "medium",
-          "The Matrix": "medium",
-          "Taken": "medium",
-          "Top Gun: Maverick": "hard",
-          "Solomon Northup": "hard",
-          "James Cameron": "hard",
-          "Patrick Bateman": "hard",
-          "Kim": "hard",
-          "Jordan Belfort": "hard",
+  "easy": [
+    "The Lion King",
+    "Pinocchio",
+    "Monsters, Inc.",
+    "The Wizard of Oz",
+    "The Breakfast Club",
+    "Finding Nemo",
+    "Finding Dory",
+    "The Iron Giant",
+    "The Avengers",
+    "E.T. the Extra-Terrestrial"
+  ],
+  "medium": [
+    "Remember the Titans",
+    "Spirited Away",
+    "Jumanji",
+    "The DeLorean",
+    "The Matrix",
+    "Taken",
+    "Interstellar",
+    "Leon: The Professional",
+    "Forrest Gump",
+    "Truman Burbank"
+  ],
+  "hard": [
+    "Avatar (2009)",
+    "Solomon Northup",
+    "James Cameron",
+    "Patrick Bateman",
+    "The Kims",
+    "Jordan Belfort",
+    "The Girl with the Dragon Tattoo",
+    "Her",
+    "Now You See Me",
+    "Blade Runners"
+  ]
 }
+
 
     
