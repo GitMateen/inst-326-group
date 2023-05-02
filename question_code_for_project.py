dd0d8fc49a9fec40c207300efcc91dab761e032b
Romeo Alvarenga <romeo.alvarenga1@gmail.com>
2023-05-01 20:59:47 -0400

    New fixed

--- question_code_for_project.py
+++ question_code_for_project.py
@@ -4,7 +4,12 @@ questions = {
                 "What movie is based on the story of a wooden puppet who dreams of becoming a real boy?",
                 "What movie follows the adventures of a group of monsters who work at a company that scares children for a living?",
                 "Which movie features a girl who travels over a rainbow to a magical land called Oz?",
-                "Which movie features a group of high school students who spend a day in detention together?"
+                "Which movie features a group of high school students who spend a day in detention together?",
+                "What movie tells the story of a clownfish named Marlin searching for his son Nemo?",
+                "In what movie does a forgetful fish named Dory go on a journey to find her family?",
+                "What movie is about a young boy who finds a giant robot from outer space and helps him return home?",
+                "What movie features a team of superheroes, including Iron Man, Captain America, and Thor, who come together to save the world?",
+                "What movie is about a friendly alien who is stranded on Earth and befriends a young boy named Elliott (hint, it is written as *letter.*letter.)?"
             ],
             "medium": [
                 "What movie is based on the true story of an African-American football coach who takes over a high school team in Virginia during segregation?",
@@ -12,7 +17,11 @@ questions = {
                 "In what movie does a group of teenagers in detention discover a mysterious board game that transports them into a jungle adventure?",
                 "In the movie Back to the Future, what is the name of the time machine?",
                 "Which movie features a character named Neo, who discovers he is living in a simulated reality?", 
-                "Which movie features a retired CIA agent who must protect his family from a group of assassins?"
+                "Which movie features a retired CIA agent who must protect his family from a group of assassins?",
+                "What movie tells the story of a group of astronauts who travel through a wormhole in search of a new home for humanity?",
+                "In what movie does a young girl named Mathilda seek revenge against the corrupt DEA agent who killed her family?",
+                "What movie is about a man named Forrest Gump and his journey through life, from a low IQ individual to a war hero and a successful businessman?",
+                "In the movie The Truman Show, what is the name of the character played by Jim Carrey, who discovers his entire life is a TV show?"
             ],
             "hard": [
                 "What was the top grossing movie in 2022?",
@@ -20,27 +29,53 @@ questions = {
                 "Who directed Avatar the movie (2009)?",
                 "In the movie American Psycho, what is the name of the character played by Christian Bale?",
                 "In the movie Parasite, what is the name of the family who cons their way into working for a wealthy family?",
-                "In the movie The Wolf of Wall Street, what is the name of the character played by Leonardo DiCaprio?"
+                "In the movie The Wolf of Wall Street, what is the name of the character played by Leonardo DiCaprio?",
+                "What movie tells the story of a young hacker named Lisbeth Salander who investigates a wealthy family's dark past?",
+                "In what movie does a man named Theodore fall in love with an artificially intelligent operating system named Samantha?",
+                "What movie is about a group of magicians who perform bank heists during their shows?",
+                "In the movie Blade Runner, what is the name of the special police unit tasked with hunting down rogue replicants?"
             ]
         }
 answers = {
-          "The Lion King": "easy",
-          "Pinocchio": "easy",
-          "Monsters, Inc.": "easy",
-          "The Wizard of Oz": "easy",
-          "The Breakfast Club": "easy",
-          "Remember the Titans": "medium",
-          "Spirited Away": "medium",
-          "Jumanji": "medium",
-          "The DeLorean": "medium",
-          "The Matrix": "medium",
-          "Taken": "medium",
-          "Top Gun: Maverick": "hard",
-          "Solomon Northup": "hard",
-          "James Cameron": "hard",
-          "Patrick Bateman": "hard",
-          "Kim": "hard",
-          "Jordan Belfort": "hard",
-}
-
+            "easy": {
+                "What movie features a young lion named Simba and his journey to become king of the Pride Lands?": "The Lion King",
+                "What movie is based on the story of a wooden puppet who dreams of becoming a real boy?": "Pinocchio",
+                "What movie follows the adventures of a group of monsters who work at a company that scares children for a living?": "Monsters Inc",
+                "Which movie features a girl who travels over a rainbow to a magical land called Oz?": "The Wizard of Oz",
+                "Which movie features a group of high school students who spend a day in detention together?": "The Breakfast Club",
+                "What movie tells the story of a clownfish named Marlin searching for his son Nemo?": "Finding Nemo",
+                "In what movie does a forgetful fish named Dory go on a journey to find her family?": "Finding Dory",
+                "What movie is about a young boy who finds a giant robot from outer space and helps him return home?": "The Iron Giant",
+                "What movie features a team of superheroes, including Iron Man, Captain America, and Thor, who come together to save the world?": "The Avengers",
+                "What movie is about a friendly alien who is stranded on Earth and befriends a young boy named Elliott (hint, it is written as *letter.*letter.)?": "E.T."
+                
+            },
     
+            "medium": {
+                "What movie is based on the true story of an African-American football coach who takes over a high school team in Virginia during segregation?": "Remember the Titans",
+                 "What movie follows the story of a young girl named Chihiro who is transported to a magical world where she must work in a bathhouse for spirits to save her parents?": "Spirited Away",
+                "In what movie does a group of teenagers in detention discover a mysterious board game that transports them into a jungle adventure?": "Jumanji",
+                "In the movie Back to the Future, what is the name of the time machine?": "The DeLorean",
+                "Which movie features a character named Neo, who discovers he is living in a simulated reality?": "The Matrix",
+                "Which movie features a retired CIA agent who must protect his family from a group of assassins?": "Taken",
+                "What movie tells the story of a group of astronauts who travel through a wormhole in search of a new home for humanity?": "Intersteller",
+                "In what movie does a young girl named Mathilda seek revenge against the corrupt DEA agent who killed her family?": "Lean The Professional",
+                "What movie is about a man named Forrest Gump and his journey through life, from a low IQ individual to a war hero and a successful businessman?": "Forrest Gump", 
+                "In the movie The Truman Show, what is the name of the character played by Jim Carrey, who discovers his entire life is a TV show?": "Truman Burbank"
+
+            },
+    
+            "hard": {
+                "What was the top grossing movie in 2022?": "Top Gun: Maverick",
+                "Who is the character in the movie 12 years a slave who was a free man but kidnapped and sold into slavery?": "Solomon Northup",
+                "Who directed Avatar the movie (2009)?": "James Cameron",
+                "In the movie American Psycho, what is the name of the character played by Christian Bale?": "Patrick Bateman",
+                "In the movie Parasite, what is the name of the family who cons their way into working for a wealthy family?":  "Kim",
+                "In the movie The Wolf of Wall Street, what is the name of the character played by Leonardo DiCaprio?": "Jordan Belfort",
+                "What movie tells the story of a young hacker named Lisbeth Salander who investigates a wealthy family's dark past?": "The Girl with the Dragon Tattoo",
+                "In what movie does a man named Theodore fall in love with an artificially intelligent operating system named Samantha?": "Her",
+                "What movie is about a group of magicians who perform bank heists during their shows?": "Now You See Me",
+                "In the movie Blade Runner, what is the name of the special police unit tasked with hunting down rogue replicants?": "Blade Runners"
+            
+}}
+ 
