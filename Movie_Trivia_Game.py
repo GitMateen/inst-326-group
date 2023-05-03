a3096228fd3946c2b38ae805dcf59b6ac297548a
Romeo Alvarenga <romeo.alvarenga1@gmail.com>
2023-05-03 11:41:06 -0400

    Removed the extra play again option, just has one now

--- Movie_Trivia_Game.py
+++ Movie_Trivia_Game.py
@@ -2,23 +2,8 @@ import random
 from question_code_for_project import questions, answers
 
 class TriviaGame:
-    """Class representing a Trivia Game.
-    
-    Attributes:
-        questions (dict): A dictionary of movie questions.
-        score1 (int): Player's 1 score.
-        score2 (int): Player's 2 score.
-        player1 (str): Player's 1 name.
-        player2 (str): Player's 2 name.
-        continue_playing (boolean): Option to continue playing the Trivia Game or not.
-    """
     #open question
     def __init__(self):
-        """Initializes, sets attributes.
-        
-        Side effects:
-            Sets attributes.
-        """
         self.questions = questions
         self.score1 = 0
         self.score2 = 0
@@ -27,11 +12,6 @@ class TriviaGame:
         self.continue_playing = True
  # start the game and enter name and level
     def start(self):
-        """Starts the game by taking the players names and difficulty. 
-        
-        Side effects:
-            Changes player1's and player2's attributes.
-        """
         self.player1 = input("Hello, welcome to our Movie Trivia game where we will test your knowledge. Start by entering player 1's name: ")
         self.player2 = input("Player 2 enter your name: ")
         level = input("Enter the level that you both want to play on (easy, medium, hard): ")
@@ -41,69 +21,45 @@ class TriviaGame:
         
     # play the game for 5 rounds and keeps score
     def play(self, level):
-        """Starts playing the game to both players. Plays the game for 5 rounds, for each player.
-        
-        Args:
-            level (str): the difficulty the players chose (easy, medium, or hard).
-        
-        Side effects:
-             Prints to stdout        
-        """
         asked_questions = []
         for i in range(5):
-                if not self.continue_playing:
-                    break
-                # Randomly select an unasked question from the chosen level's dictionary and ask the first player
-                question1 = self.get_unasked_question(level, asked_questions)
-                asked_questions.append(question1)
-                print(f"{self.player1}, {question1}")
-                answer1 = input("Your answer: ")
-                if answer1.lower() == answers[level][question1].lower():
-                    print("Correct!")
-                    self.score1 += 1
-                else:
-                    print(f"Incorrect. The correct answer is {answers[level][question1]}")
-                if not self.continue_playing:
-                    break
-                # Randomly select an unasked question from the same level's dictionary and ask the second player
-                question2 = self.get_unasked_question(level, asked_questions)
-                asked_questions.append(question2)
-                print(f"{self.player2}, {question2}")
-                answer2 = input("Your answer: ")
-                if answer2.lower() == answers[level][question2].lower():
-                    print("Correct!")
-                    self.score2 += 1
-                else:
-                    print(f"Incorrect. The correct answer is {answers[level][question2]}")
-                    
-                # Print the current scores
-                print(f"Current Scores:\n{self.player1}: {self.score1}\n{self.player2}: {self.score2}")
+            if not self.continue_playing:
+                break
+            # Randomly select an unasked question from the chosen level's dictionary and ask the first player
+            question1 = self.get_unasked_question(level, asked_questions)
+            asked_questions.append(question1)
+            print(f"{self.player1}, {question1}")
+            answer1 = input("Your answer: ")
+            if answer1.lower() == answers[level][question1].lower():
+                print("Correct!")
+                self.score1 += 1
+            else:
+                print(f"Incorrect. The correct answer is {answers[level][question1]}")
+            if not self.continue_playing:
+                break
+            # Randomly select an unasked question from the same level's dictionary and ask the second player
+            question2 = self.get_unasked_question(level, asked_questions)
+            asked_questions.append(question2)
+            print(f"{self.player2}, {question2}")
+            answer2 = input("Your answer: ")
+            if answer2.lower() == answers[level][question2].lower():
+                print("Correct!")
+                self.score2 += 1
+            else:
+                print(f"Incorrect. The correct answer is {answers[level][question2]}")
                 
-                # Check if the players want to continue playing
-                continue_input = input("Do you want to continue playing? (y/n) ")
-                self.continue_playing = continue_input.lower() == 'y'
+        # Print the final scores
+        print(f"Final Scores:\n{self.player1}: {self.score1}\n{self.player2}: {self.score2}")
+        
      
     def get_unasked_question(self, level, asked_questions):
         unasked_questions = [question for question in self.questions[level] if question not in asked_questions]
         return random.choice(unasked_questions)
                 
 class Scores:       
-    """Class representing the score of a player
-    
-    Attributes:
-        score (int): a player's score.
-    """ 
+    """Class representing the score of a player"""  
     def __init__(self, score):
-        """Initializes, sets attributes.
-        
-        Args:
-            score (int): a player's score.
-        
-        Side effects:
-            Sets attributes.
-        """
         self.score = score
-       
             
     def __add__(self, other):
         """Adds a players score with a number.
@@ -112,7 +68,7 @@ class Scores:
             other (int): number being added to a player's score.
         """  
         combined_score = self.score + other
-        return combined_score
+        return Scores(combined_score)
     
     def __sub__(self, other):
         """Subtracts from a players score
@@ -120,7 +76,7 @@ class Scores:
             other (int): number being used to subtract from the player's score.
         """
         removed_points = self.score - other
-        return removed_points
+        return Scores(removed_points)
     
     
 class Player:
@@ -149,8 +105,6 @@ class Player:
         """
         When player_one and player_two have the same score at the end of all the rounds. 
         There is one final question break the tie and decide a winner.
-        There are 11 questions per game round. 5 rounds so 10 qeustions asked. if we need a tiebreaker,
-        we will ask the 11th questions that has not been asked yet.
         """
         pass
 
@@ -165,9 +119,14 @@ class Player:
         #we need 6 questions for easy mode. we only have 5 questions. imma add one more question later
         pass
 
+    
 play_again = True
+
 while play_again:
     game = TriviaGame()
     game.start()
     play_again_input = input("Do you want to play again? (y/n) ")
     play_again = play_again_input.lower() == 'y'
+    
+if __name__ == "__main__":
+    pass
