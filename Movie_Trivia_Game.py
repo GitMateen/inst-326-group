14678b1530d7b02ca091a72f225b1a699b4483f5
Romeo Alvarenga <romeo.alvarenga1@gmail.com>
2023-05-03 11:14:28 -0400

    if adding more questions, do 'question':'answer' in the answers dictionary.
    '

--- Movie_Trivia_Game.py
+++ Movie_Trivia_Game.py
@@ -165,16 +165,11 @@ class Player:
         #we need 6 questions for easy mode. we only have 5 questions. imma add one more question later
         pass
 
-<<<<<<< HEAD
-
-
 play_again = True
 while play_again:
     game = TriviaGame()
     game.start()
     play_again_input = input("Do you want to play again? (y/n) ")
     play_again = play_again_input.lower() == 'y'
-=======
 if __name__ == "__main__":
     pass
->>>>>>> b518f0e (updates)
